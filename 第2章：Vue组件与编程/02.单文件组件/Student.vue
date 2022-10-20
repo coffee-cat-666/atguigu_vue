ee2dfbb055429ad6a0d76f5e6a4d3e7ff534985b
laowang <hellogit@laowang.cn>
2022-10-20 22:33:33 +0800

    10/20

---
+++ 第2章：Vue组件与编程/02.单文件组件/Student.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="demo">
+    <h2>学生姓名:{{ studentName }}</h2>
+    <h2>年龄:{{ age }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Studnet",
+  data: {
+    return: {
+      studnetName: "老王",
+      age: 19,
+    },
+  },
+};
+</script>
+
+<style>
+.demo {
+  background: red;
+  color: pink;
+  font-size: 20px;
+}
+</style>
